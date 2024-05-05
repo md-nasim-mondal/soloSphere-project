6364310daa9b60c5f18112bef02fa23e79854774
Nasim <nasim.programmer24.com>
2024-05-05 18:53:36 +0600

    completed the first conceptional of milestone 11 and storing it on github

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+import Carousel from "../components/Carousel";
+import TabCategories from "../components/TabCategories";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Carousel/>
+            <TabCategories />
+        </div>
+    );
+};
+
+export default Home;
